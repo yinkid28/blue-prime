3ee7006f806f96fc78855d0a3fec5e674363556e
Elyzsium <fadiperukayat@gmail.com>
2024-08-31 21:32:07 +0100

    update fixed

---
+++ src/models/webber.model.ts
@@ -0,0 +1,13 @@
+export type RequestCardProps = {
+    title: string;
+    children: React.ReactNode;
+  };
+
+  export type tableTypes = {
+    date: string;
+    appName: string;
+    plan: string;
+    price: string;
+    paymentMethod: string;
+    paymentStatus: string;
+  };
