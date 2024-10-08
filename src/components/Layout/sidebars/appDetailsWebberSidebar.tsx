601218b2161ec47943138f62e3363fa04f815d7f
Elyzsium <fadiperukayat@gmail.com>
2024-10-08 14:58:49 +0100

    update

--- src/components/Layout/sidebars/appDetailsWebberSidebar.tsx
+++ src/components/Layout/sidebars/appDetailsWebberSidebar.tsx
@@ -2,32 +2,31 @@ import { useApi } from "@/context/ApiDiscoveryContext";
 import { useOnboarding } from "@/context/OnboardingContext";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 import { FaChevronLeft } from "react-icons/fa";
 
 export default function AppDetailsWebberSidebar() {
   const router = useRouter();
   const { setSidebar } = useOnboarding();
   const { currentApplication } = useApi();
+  const [activeItem, setActiveItem] = useState("oauth");
 
-
-  const handleNavigation = (path: string) => {
+  const handleNavigation = (path: string, item: string) => {
     router.push(path);
+    setActiveItem(item);
   };
 
-  const getItemStyle = (path: string) => {
-    const isActive = router.asPath === path;
-    return `md:flex hidden items-center cursor-pointer hover:text-primary gap-3 w-full ${
-      isActive
-        ? "text-primary"
-        : "text-dark-grey"
+  const getItemStyle = (item: string) => {
+    const isActive = activeItem === item;
+    return `md:flex items-center cursor-pointer hover:text-primary gap-3 w-full ${
+      isActive ? "text-primary" : "text-dark-grey"
     }`;
   };
 
   return (
     <div className="bg-white rounded p-5 h-full flex flex-col gap-4 ">
       <Image
-        src={"/icons/logo.svg"}
+        src="/icons/logo.svg"
         alt="logo"
         width={200}
         height={100}
@@ -44,20 +43,18 @@ export default function AppDetailsWebberSidebar() {
         <p className="text-mid-grey">Back</p>
       </div>
 
-      <div        
-      >
-      <p className="font-semibold">{currentApplication!.name}</p>
-      </div>
+      {currentApplication && (
+        <div
+          className={getItemStyle("oauth")}
+          onClick={() => handleNavigation(`/webber/library/application/${currentApplication.name}`, "oauth")}
+        >
+          <p className="font-semibold">OAuth API Keys</p>
+        </div>
+      )}
 
       <div
-        className={getItemStyle("")}
-       
-      >
-        <p className="font-semibold">OAuth API Keys</p>
-      </div>
-      <div
-        className={getItemStyle("webber/library")}
-        onClick={() => handleNavigation("/webber/library/${api.apiCode}/subscriptions")}
+        className={getItemStyle("subscriptions")}
+        onClick={() => handleNavigation(`/webber/library/${currentApplication?.apiCode}/subscriptions`, "subscriptions")}
       >
         <p className="font-semibold">Subscriptions</p>
       </div>
